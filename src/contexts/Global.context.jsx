6af2c90cf548e2064640b0de734e99b11b43cbd4
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-01-10 22:25:36 +0500

    added local storage functionality fo persist data

--- src/contexts/Global.context.jsx
+++ src/contexts/Global.context.jsx
@@ -1,14 +1,17 @@
 import React, { createContext, useReducer, useEffect } from "react";
 import { globalReducer } from "../reducers/global.reducer";
 const initialState = {
-  todos: [],
+  todos: [
+    { id: 1, title: "Play Guitar", completed: false },
+    { id: 2, title: "Watch Netflix new Series", completed: true },
+  ],
 };
 export const GlobalContext = createContext(initialState);
 
 const GlobalContextProvider = ({ children }) => {
   const [todos, dispatch] = useReducer(globalReducer, initialState, () => {
     return JSON.parse(
-      localStorage.getItem("todos") || JSON.parse({ todos: [] })
+      localStorage.getItem("todos") || JSON.stringify(initialState)
     );
   });
   useEffect(() => {
